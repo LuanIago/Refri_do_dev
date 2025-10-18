ea091b4e329eee5e303d83b02ff5a2da179d5f8c
LuanIago <luaniagoaguiarmarques@gmail.com>
2025-10-18 00:37:09 -0300

    Manipulated website with JavaScript

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+let list = document.querySelectorAll('.item');
+let buttonRight = document.querySelector('.arrow-right');
+let buttonLeft = document.querySelector('.arrow-left');
+
+let count = list.length;
+let active = 0;
+
+buttonRight.addEventListener('click', () => {
+    let activeOld = document.querySelector('.active');
+    activeOld.classList.remove('active');
+
+    active = active < count - 1 ? active + 1 : 0;
+    list[active].classList.add('active');
+});
+
+buttonLeft.addEventListener('click', () => {
+    let activeOld = document.querySelector('.active');
+    activeOld.classList.remove('active');
+
+    active = active > 0 ? active - 1 : count - 1;
+    list[active].classList.add('active');
+});
